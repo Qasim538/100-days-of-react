6a6e93b157a8d2f3064cd7fdb180af1b2f6f22b6
Qasim <mq12113@gmail.com>
2024-12-26 13:58:29 +0000

    updated menu

---
+++ 45-food-menu-dynamic-filter/src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+
+import { useState } from "react";
+import items from "./Components/data";
+import Menu from "./Components/Menu";
+import Categories from "./Components/Categories";
+
+const allCategories = ['all', ...new Set(items.map((item)=>item.category))] 
+console.log(allCategories);
+
+
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
