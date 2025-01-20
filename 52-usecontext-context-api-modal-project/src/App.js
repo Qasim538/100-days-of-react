29e526b4588e06a0c29d7c414f7b53ff9e4650da
Qasim <mq12113@gmail.com>
2025-01-20 21:50:22 +0000

    completed project 52 with Context Api

---
+++ 52-usecontext-context-api-modal-project/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './Components/Home'
+import Modal from './Components/Modal'
+import Sidebar from './Components/Sidebar'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </div>
+  )
+}
+
+export default App
