c89bd33648ca1eed90568914c8634f52f495823a
Qasim <mq12113@gmail.com>
2025-02-12 07:56:47 +0000

    reducer set up

---
+++ 54-shopping-cart/src/data.js
@@ -0,0 +1,23 @@
+export default [
+    {
+      id: 1,
+      title: 'Samsung Galaxy S7',
+      price: 599.99,
+      img: 'https://cdn.alloallo.media/catalog/product/samsung/galaxy-s/galaxy-s7/galaxy-s7-gold-platinum.jpg',
+      amount: 1,
+    },
+    {
+      id: 2,
+      title: 'google pixel ',
+      price: 499.99,
+      img: 'https://lh3.googleusercontent.com/KaLIFYVg9298b8jv33H3pagRaAz4lCQxrQz-goMEsiTuCmUf2Ood9ktkzgjpotkMuRcAMimOV2RfN7vBZVmnInf5wcwUNsRZpw',
+      amount: 1,
+    },
+    {
+      id: 3,
+      title: 'Xiaomi Redmi Note 2',
+      price: 699.99,
+      img: 'https://imei24.com/img/xiaomi/13_53_13_2.jpg',
+      amount: 1,
+    },
+  ];
